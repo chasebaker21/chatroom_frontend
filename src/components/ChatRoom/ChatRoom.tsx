8080b47b973f851ee0d898e440e9e3a118c55907
Chase Baker <chasebaker@Chases-MBP-2.attlocal.net>
2023-12-18 08:36:32 -0500

    started work on chat front end

---
+++ src/components/ChatRoom/ChatRoom.tsx
@@ -0,0 +1,6 @@
+import "./ChatRoom.scss"
+import {ChatBox} from "../../common/components/ChatBox/ChatBox";
+
+export const ChatRoom = () => {
+    return (<div><h1>Chat Room</h1> <ChatBox/></div>)
+}
